aa159d1c1b4e4b3cd0017f6013cf573abb4b47d6
Julien Gosselet <gosseletjuju@gmail.com>
2024-09-15 20:03:22 +0200

    Ajout du php et css de base

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Bibliothèque Serrano</title>
+    <link rel="stylesheet" href="styleadmin.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+CU:wght@100..400&display=swap" rel="stylesheet">
+</head>
+</html>
