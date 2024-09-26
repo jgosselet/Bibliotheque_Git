3022efa9e51bce8b917529deb879f327ec1ab140
jgosselet <gosseletjuju@gmail.com>
2024-09-26 10:40:14 +0200

    Fichier qui déconnecte

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); // Démarrer la session
 session_destroy(); // Détruire toutes les données de session
-header("Location: index.php"); // Rediriger vers la page de connexion ou d'accueil
+header("Location: index.php"); // Rediriger vers la page de connexion 
 exit();
 ?>
